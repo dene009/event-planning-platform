fbd0d303d918294bb9bd17d5703c4698002340d1
dene009 <esidene009@gmail.com>
2025-01-04 15:52:09 +0000

    Navigation update

--- eventbridge/src/components/Navigation/Navigation.jsx
+++ eventbridge/src/components/Navigation/Navigation.jsx
@@ -1,24 +1,27 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
 
-const Navigation = ()=>{
+const Navigation = () => {
   return (
     <nav>
-        <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/find-events">Find Events</Link>
-            </li>
-            <li>
-              <Link to="/register/">Register</Link>
-            </li>
-            <li>
-              <Link to="/login/">Login</Link>
-            </li>
-        </ul>
+      <ul>
+        <li className="brand-name">EventBridge</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/find-events">Find Events</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
+
 export default Navigation;
+
